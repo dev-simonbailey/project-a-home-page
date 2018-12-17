827431bf7c5f15be3b8e3436327244af70f80490
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2018-12-17 17:07:38 +0000

    Update index.php

--- index.php
+++ index.php
@@ -3,3 +3,5 @@
   <img src='<?=$vendorURL?>images/aprilia-logo.jpg' width="200">
   <br />
   <img src='<?=$vendorURL?>images/Ducati_Logo1.png' width="200">
+ <br />
+ <img src='<?=$vendorURL?>images/mv_agusta.jpg' width="200">
