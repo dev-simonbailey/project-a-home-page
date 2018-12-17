179423cdfb10a35c8fc2c416e6c0d4e8a81311be
Simon Bailey <30531868+dev-simonbailey@users.noreply.github.com>
2018-12-17 17:05:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
   <h1>Project A</h1>
   <img src='<?=$vendorURL?>images/aprilia-logo.jpg' width="200">
   <br />
-  <img src='<?=$vendorURL?>images/aprilia-logo.jpg' width="200">
+  <img src='<?=$vendorURL?>images/Ducati_Logo1.jpg' width="200">
